4ac894601441cdf566903efea78a9918ea0b7ac8
Muhammad Brilian Erranaomi <brilianeranaomi@gmail.com>
2026-05-06 13:09:13 +0700

    add instruct

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
-#include <Arduino.h>   // WAJIB
+#include <Arduino.h> // WAJIB
 #include "gateway.h"
 
-void setup() {
+// Buat handling module Ebyte E220 header di folder include, dan file .cpp di folder cpp
+
+void setup()
+{
   Serial.begin(115200);
   setupGateway();
 }
 
-void loop() {
+void loop()
+{
   loopGateway();
+  // loop event membaca incoming dari module, if (e220ttl.available()>1) dst
+  // jika data berhasil diterima kirim ke httpserver misal pakai google.com asal kirim dulu
 }
